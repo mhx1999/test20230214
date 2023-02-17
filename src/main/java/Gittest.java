daf898b6d7f9aa9704c9826136b68ffcfb991023
mhx <1282488032@qq.com>
2023-02-17 19:38:19 +0800

    hot-fix commit

--- src/main/java/Gittest.java
+++ src/main/java/Gittest.java
@@ -2,5 +2,7 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("gook lack");
+        System.out.println("third");
+        System.out.println("666");
     }
 }
