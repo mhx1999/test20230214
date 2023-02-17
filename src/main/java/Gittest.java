cdb94444596487a5530d230618573e042555c5f6
mhx <1282488032@qq.com>
2023-02-17 18:35:52 +0800

    second commit fix

--- src/main/java/Gittest.java
+++ src/main/java/Gittest.java
@@ -2,5 +2,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("gook lack");
+        System.out.println("third");
     }
 }
